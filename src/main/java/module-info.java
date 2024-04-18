abb80862fdfd86bbb8bf77e0561586994a6bcbf6
bruno bispo <bbispo.by@gmail.com>
2024-04-17 23:27:54 -0300

    Revert "Modelando em MVC"
    This reverts commit 107d43c6582ff52db3c653c067c343269d8c4fbe.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,6 @@ module org.quijava.quijava {
     requires javafx.fxml;
 
 
-    opens org.quijava.views to javafx.fxml;
-    exports org.quijava.views;
-    exports org.quijava.controllers;
-    opens org.quijava.controllers to javafx.fxml;
+    opens org.quijava.quijava to javafx.fxml;
+    exports org.quijava.quijava;
 }
--- src/main/java/org/quijava/quijava/HelloController.java
+++ src/main/java/org/quijava/quijava/HelloController.java
@@ -1,4 +1,4 @@
-package org.quijava.controllers;
+package org.quijava.quijava;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
