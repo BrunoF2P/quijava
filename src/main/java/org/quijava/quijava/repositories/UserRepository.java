c4462305ce39724f2948c4265835b8381c7f6ea6
BrunoF2P <brunobispof2p@gmail.com>
2024-05-06 23:13:53 -0300

    repositorio do usuario

---
+++ src/main/java/org/quijava/quijava/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package org.quijava.quijava.repositories;
+
+
+import org.quijava.quijava.models.UserModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Integer> {
+
+    boolean existsByUsername(String username);
+
+}
