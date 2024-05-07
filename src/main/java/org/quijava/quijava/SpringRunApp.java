4c14929701027493eb52d28582bb244d21fbdaac
BrunoF2P <brunobispof2p@gmail.com>
2024-05-06 23:09:03 -0300

    inicalizador spring boot pra usar recursos do spring data

---
+++ src/main/java/org/quijava/quijava/SpringRunApp.java
@@ -0,0 +1,14 @@
+package org.quijava.quijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringRunApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
