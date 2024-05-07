40f51585a0964d6a143330d5b7c11ec0ad8f9458
BrunoF2P <brunobispof2p@gmail.com>
2024-05-06 23:14:42 -0300

    encriptador de senhas

---
+++ src/main/java/org/quijava/quijava/utils/PasswordEncoder.java
@@ -0,0 +1,18 @@
+package org.quijava.quijava.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordEncoder {
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public String encodePassword(String password) {
+        return encoder.encode(password);
+    }
+
+    public boolean matches(String rawPassword, String encodedPassword) {
+        return encoder.matches(rawPassword, encodedPassword);
+    }
+}
