73bbcbdc5357b3ab832dfa064913e55fb9bb0965
BrunoF2P <brunobispof2p@gmail.com>
2024-05-12 20:03:18 -0300

    add: Criar Quiz

---
+++ src/main/java/org/quijava/quijava/controllers/CreateQuizController.java
@@ -0,0 +1,10 @@
+package org.quijava.quijava.controllers;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+@Component
+public class CreateQuizController {
+
+}
