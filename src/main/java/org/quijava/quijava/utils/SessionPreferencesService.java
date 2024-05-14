13539e72305ab28d9b7dca3a83ddfc3e9fbdbf71
BrunoF2P <brunobispof2p@gmail.com>
2024-05-14 18:01:08 -0300

    Armazenando rota de usuario nas preferencias

--- src/main/java/org/quijava/quijava/utils/SessionPreferencesService.java
+++ src/main/java/org/quijava/quijava/utils/SessionPreferencesService.java
@@ -5,6 +5,7 @@ import java.util.prefs.Preferences;
 public class SessionPreferencesService {
     private static final String USERNAME_KEY = "username";
     private static final String SESSION_ID_KEY = "session_id";
+    private static final String SESSION_ROLE_KEY = "role";
 
     private final Preferences preferences;
 
@@ -15,6 +16,7 @@ public class SessionPreferencesService {
     public void setUsername(String username) {
         preferences.put(USERNAME_KEY, username);
     }
+
 
     public String getUsername() {
         return preferences.get(USERNAME_KEY, null);
@@ -28,8 +30,18 @@ public class SessionPreferencesService {
         return preferences.getInt(SESSION_ID_KEY, -1);
     }
 
+
+    public void setRole(int role) {
+        preferences.putInt(SESSION_ROLE_KEY, role);
+    }
+
+    public int getRole() {
+        return preferences.getInt(SESSION_ROLE_KEY, -1);
+    }
+
     public void clear() {
         preferences.remove(USERNAME_KEY);
         preferences.remove(SESSION_ID_KEY);
+        preferences.remove(SESSION_ROLE_KEY);
     }
 }
